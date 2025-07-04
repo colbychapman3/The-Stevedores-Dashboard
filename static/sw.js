bb015d2d82b04bfd2a6adbbad27d212ad4680f2a
colbychapman3 <colbychapman3@gmail.com>
2025-07-04 00:10:20 -0400

    Add PWA static files and deployment configuration

---
+++ static/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'stevedores-dashboard-v1';
+const urlsToCache = [
+  '/',
+  '/ships',
+  '/containers',
+  '/crew',
+  '/static/manifest.json',
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
